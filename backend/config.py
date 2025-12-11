03df3bbeeab69542a1e2902b3513c96d27db4d71
Honore <harerimana456@gmail.com>
2025-12-11 12:56:05 +0200

    updated my database connection

--- backend/config.py
+++ backend/config.py
@@ -4,15 +4,7 @@ import os
 load_dotenv()
 
 class Config:
-    USER = os.getenv("USER")
-    PASSWORD = os.getenv("PASSWORD")  # this should already be URL-encoded in .env
-    HOST = os.getenv("HOST")
-    PORT = os.getenv("PORT")
-    DBNAME = os.getenv("DBNAME")
     SECRET_KEY = os.getenv("SECRET_KEY")
 
-    SQLALCHEMY_DATABASE_URI = (
-        f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
-    )
-
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")  # direct full URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
